759c4b2f18f5e607682129d15c8a6c7d9648c821
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-27 15:49:14 -0400

    doc: att

--- envioEmail.py
+++ envioEmail.py
@@ -7,11 +7,13 @@ from src.enviarEmail.email import enviar_email, assunto, de, para, senha
 inicioContador = time.time()
 caminhoPastaLimp = r'\\192.168.1.2\dados\SUPERMERCADO ACOMPANHAMENTO\RESULTADOS CLIENTES - ALEX 2024.xlsx'
 caminhoForge = r'\\192.168.1.2\dados\SUPERMERCADO CONTABIL\Planilhas resultados - Power Bi'
-ondeSalvar = r'C:\Users\User\Documents\GitHub\data-analytics-back-end\src\ArquivosDRE\\'
+
+ondeSalvarLimpPlan = r'C:\Users\User\Documents\GitHub\data-analytics-back-end\src\ArquivoResCliente\\'
+ondeSalvarForge = r'C:\Users\User\Documents\GitHub\data-analytics-back-end\src\ArquivosDRE\\'
 
 print('Iniciando as analises...')
-tempoLimp = limp_plan(caminhoPastaLimp, ondeSalvar)
-tempoForge = forgePlan(caminhoForge, ondeSalvar, 2024, 1)
+tempoLimp = limp_plan(caminhoPastaLimp, ondeSalvarLimpPlan)
+tempoForge = forgePlan(caminhoForge, ondeSalvarForge, 2024, 1)
 
 corpo_mensagem = f'''<p>Olá, segue em anexo o resultado do processo de limpeza e da planilha DRE.</p>
 <p>Planilha Resultado Cliente<br>{tempoLimp}</p>
