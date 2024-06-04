6d10d71b4e4f58896b5641610427882cd1cf55a5
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-04 08:30:42 -0400

    doc: att

--- src/CmdSQL/limpar_cmd_sql.py
+++ src/CmdSQL/limpar_cmd_sql.py
@@ -1,10 +1,26 @@
-import pandas as pd
 cmd_sql = []
+dict_cmd = {}
+newCMD = []
+
 for i in open('LogComandos.sql','r'):
     cmd_sql.append(i.split(' '))
 
-with open('ComandosLog.txt','a') as f:
-    for lin in cmd_sql:
-        for i in range(2, len(lin)):
-            f.write(f' {lin[i]}')
+for key, lin in enumerate(cmd_sql):
+    for i in range(2, len(lin)):
+        if i == len(lin)-1:
+           newCMD.append(f'{str(lin[i]).replace('\n',';\n')}')
+        else:
+            newCMD.append(f'{lin[i]}')
+    dict_cmd[key] = newCMD.copy()
+    newCMD.clear()
 
+# with open('ComandosLog.txt','a') as f:
+#     for lin in cmd_sql:
+#         for i in range(2, len(lin)):
+#             if i == len(lin)-1:
+#                 f.write(f' {lin[i]};')
+#                 break
+#             else:
+#                 f.write(f' {lin[i]}')
+
+lol='lol'
